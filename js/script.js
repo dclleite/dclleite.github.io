6bac50885efa4afd1bfb89777e0f4463a2a17564
Eduardo Carvalho <eduappsplay@gmail.com>
2019-02-22 11:31:49 -0300

    Scrol suave para links internos

--- js/script.js
+++ js/script.js
@@ -1,20 +1,25 @@
-$(function () {
 
-	/*var nav = $('.menu');
-	$('.btmenu').click(function(){
-		nav.toggleClass("menu-open");
-	})
-	*/
-	$('.btmenu').click(function(){
-		$("ul").slideToggle();
-
-	});
-
-
-	$(window).resize(function(){
-		if ($(window).width() > 768){
-			$("ul").removeAttr('style');
-		}
-	});
+//abrir o menu mobile
+$('.btmenu').click(function(){
+	$("ul").slideToggle();
 
 });
+
+$(window).resize(function(){
+	if ($(window).width() > 768){
+		$("ul").removeAttr('style');
+	}
+});
+
+
+$('.linkInterno, .botao').click(function(e){
+	e.preventDefault();
+	var id = $(this).attr('href'),
+		menuHeight = $('nav').innerHeight(),
+		targetOffset = $(id).offset().top;
+	$('html, body').animate({
+		scrollTop: targetOffset - menuHeight
+	}, 500);
+});
+
+
