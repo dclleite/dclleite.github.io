b3aeb160662f4ec221c301cd24b2a4e4e8f90d74
Eduardo Carvalho <eduappsplay@gmail.com>
2019-02-26 20:49:14 -0300

    adicionando formulario de contato

--- js/script.js
+++ js/script.js
@@ -4,22 +4,38 @@ $('.btmenu').click(function(){
 	$("ul").slideToggle();
 
 });
-
+//desbugar a nav quando a tela aumentar
 $(window).resize(function(){
 	if ($(window).width() > 768){
 		$("ul").removeAttr('style');
 	}
 });
 
-
-$('.linkInterno, .botao').click(function(e){
+//Scrol da página ao clicar em uma opção do menu
+$('.linkInterno').click(function(e){
 	e.preventDefault();
+
+	if ($(window).width() < 768){
+		$('#check').prop("checked",false);
+		$("ul").slideToggle();
+	}
 	var id = $(this).attr('href'),
-		menuHeight = $('nav').innerHeight(),
+		menuHeight = $('nav div').innerHeight(),
 		targetOffset = $(id).offset().top;
 	$('html, body').animate({
 		scrollTop: targetOffset - menuHeight
 	}, 500);
+
 });
+// scroll da página ao clicar em uma opção do botão
+$('.botao').click(function(e){
+	e.preventDefault();
 
+	var id = $(this).attr('href'),
+		menuHeight = $('nav div').innerHeight(),
+		targetOffset = $(id).offset().top;
+	$('html, body').animate({
+		scrollTop: targetOffset - menuHeight
+	}, 500);
 
+});
